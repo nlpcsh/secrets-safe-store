ab3e7f99b228b33de5975dbcec31bc0e9b95a223
DESKTOP-CGVTMCG\JeDI <nikolay.petrov@abv.bg>
2020-08-26 18:11:44 +0300

    Level 4: using passport to login and have session

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-//jshint esversion:6
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
@@ -14,68 +15,93 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+// this has to be placed in that order!!!
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect('mongodb://localhost:27017/userDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true 
 });
+
+mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model(
     'User', userSchema
 );
 
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+///////////
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.render('home');
 });
 
 app.route('/login')
-    .get(function (req, res) {
-    res.render('login');
+    .get(function(req, res) {
+        res.render('login');
     })
-    .post(function (req, res) {
-        const username = req.body.username;
-        const password = md5(req.body.password);
-
-        User.findOne({email: username}, function(err, foundUser) {
+    .post(function(req, res) {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
+        req.login(user, function(err) {
             if (err) {
                 console.log(err);
             } else {
-                if (foundUser) {
-                    if (foundUser.password === password) {
-                        res.render('secrets');
-                    }
-                }
+                passport.authenticate('local')(req, res, function() {
+                    res.redirect('/secrets');
+                });
             }
-        })
+        });
     });
 
 app.route('/register')
-    .get(function (req, res) {
+    .get(function(req, res) {
         res.render('register');
     })
     .post(function(req, res) {
-        const newUser = new User({
-            email: req.body.username,
-            password: md5(req.body.password)
-        });
-
-        newUser.save(function (err) { 
-            if (!err) {
-                res.render('secrets');
-            } else {
+        User.register({username: req.body.username}, req.body.password, function(err, user) {
+            if (err) {
                 console.log(err);
+                res.redirect('/register');
+            } else {
+                passport.authenticate('local')(req, res, function() {
+                    res.redirect('/secrets');
+                });
             }
         });
     });
 
+app.get('/secrets', function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render('secrets');
+    } else {
+        res.redirect('/login');
+    }
+});
 
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
 
-
-app.listen(3000, function () {
-    console.log('Server is started on port 3000');
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Server is started successfuly!');
 });
